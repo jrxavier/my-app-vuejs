d29c26faa10d16c4816812b526aab142e8a39930
José Ricardo Xavier <jrxavier@gmail.com>
2021-03-27 11:53:12 -0300

    Projeto basico inicial de app Vue configurada para Docker. Front-end com Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/logo.png" />
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div>
+      <div>
+        <b-button-group>
+          <b-button>Button 1</b-button>
+          <b-button>Button 2</b-button>
+          <b-button>Button 3</b-button>
+        </b-button-group>
+      </div>
+      <div class="mt-3">
+        <b-button-group>
+          <b-button variant="success">Success</b-button>
+          <b-button variant="info">Info</b-button>
+          <b-button variant="warning">Warning</b-button>
+        </b-button-group>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      msg: 'Welcome to Vuejs'
+    }
+  }
 }
 </script>
 
